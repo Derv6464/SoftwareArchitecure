9a51f6ce2ee3f7ce2545f881fdc9d87073c9140b
newsyd04 <newsyd04@gmail.com>
2023-05-24 16:20:48 +0100

    first push

---
+++ DarkplaceHospital/src/main/java/com/darkplace/DarkplaceHospital/DarkplaceHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.darkplace.DarkplaceHospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkplaceHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DarkplaceHospitalApplication.class, args);
+	}
+
+}
---
+++ DarkplaceHospital/src/test/java/com/darkplace/DarkplaceHospital/DarkplaceHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darkplace.DarkplaceHospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkplaceHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
