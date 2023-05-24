57fbbb2ad6a617184f5cde6d458023af11b38fd2
newsyd04 <newsyd04@gmail.com>
2023-05-24 16:37:53 +0100

    added static and templates, added sql files

---
+++ DarkplaceHospital/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO patients (id, name) VALUES (1, 'Garth');
+INSERT INTO patients (id, name) VALUES (2, 'Thornton');
+INSERT INTO patients (id, name) VALUES (3, 'Liz');
+INSERT INTO patients (id, name) VALUES (4, 'Hopper');
+INSERT INTO patients (id, name) VALUES (5, 'Sanchez');  
